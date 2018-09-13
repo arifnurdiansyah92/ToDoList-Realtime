00baf84c67ded66f82a0b046a9a2b34c849fe63c
Arif Nurdiansyah <arifnurdiansyah92@hotmail.com>
2018-09-13 11:01:42 +0700

    Tambah Data Identitas pada Model User

--- ToDoList/Models/ApplicationUser.cs
+++ ToDoList/Models/ApplicationUser.cs
@@ -9,5 +9,8 @@ namespace ToDoList.Models
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public string NamaDepan { get; set; }
+        public string NamaBelakang { get; set; }
+        public string JenisKelamin { get; set; } 
     }
 }
