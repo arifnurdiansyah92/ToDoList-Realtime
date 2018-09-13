186c424cd79dfaca1595c87b38cb29926a1c33b1
Arif Nurdiansyah <arifnurdiansyah92@hotmail.com>
2018-09-13 11:06:50 +0700

    Tambah Model Team

---
+++ ToDoList/Models/Team.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoList.Models
+{
+    public class Team
+    {
+        public int ID { get; set; }
+        public string Nama { get; set; }
+        public string Deskripsi { get; set; }
+        public ICollection<Anggota> Anggota { get; set; }
+    }
+    public class Anggota
+    {
+        public int ID { get; set; }
+        public int TeamID { get; set; }
+        public Team Team { get; set; }
+        public int UserID { get; set; }
+        public ApplicationUser User { get; set; }
+    }
+}
